4e56061bbf18f661b42d6c80eff7137ee03f98d3
MarioGomezFdez <mariogomezfdez@gmail.com>
2018-06-15 18:46:36 +0200

    both

--- src/negocio/modelos/Empleado.java
+++ src/negocio/modelos/Empleado.java
@@ -60,7 +60,7 @@ public class Empleado {
             }
         }*/
         
-        return "Supervisor";
+        return "Administrativo";
     }
     
     public String getVinculacionActual(){
